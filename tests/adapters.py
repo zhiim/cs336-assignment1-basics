32ff4f40116657444a77acb6acbf902c8c449fb7
Q Xu <xuq3196@outlook.com>
2025-11-12 21:09:26 +0800

    implement test

--- tests/adapters.py
+++ tests/adapters.py
@@ -589,4 +589,6 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    from train_bpe import train_bpe
+
+    return train_bpe(input_path, vocab_size, special_tokens, num_processes=128)
