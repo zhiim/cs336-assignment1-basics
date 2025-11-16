e729c96cd9e40bd331615d84d05fda52ae1e0fd1
Q Xu <xuq3196@outlook.com>
2025-11-16 18:09:53 +0800

    rmsnorm: finsh and pass all test

--- cs336_basics/modules.py
+++ cs336_basics/modules.py
@@ -2,7 +2,7 @@ from math import sqrt
 
 import torch
 import torch.nn as nn
-from einops import einsum
+from einops import einsum, reduce
 
 
 class Linear(nn.Module):
@@ -66,3 +66,54 @@ class Embedding(nn.Module):
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         """Lookup the embedding vectors for the given token IDs."""
         return self.embedding_map[token_ids]
+
+
+class RMSNorm(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        eps: float = 1e-5,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
+        """Construct the RMSNorm module.
+
+        Args:
+            d_model (int): Hidden dimension of the model
+            eps (float): Epsilon value for numerical stability
+            device (torch.device | None): Device to store the parameters on
+            dtype (torch.dtype | None): Data type of the parameters
+        """
+        super().__init__()
+
+        self.d_model = d_model
+        self.eps = eps
+
+        self.gain = nn.Parameter(
+            torch.ones((d_model,), device=device, dtype=dtype)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Process an input tensor of shape
+        (batch_size, sequence_length, d_model) and return a tensor of the same
+        shape.
+        """
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+
+        rms = torch.sqrt(
+            reduce(
+                einsum(x, x, "... d_model, ... d_model -> ... d_model"),
+                "... d_model -> ...",
+                "mean",
+            )
+            + self.eps
+        )
+
+        normed = einsum(
+            einsum(x, 1 / rms, "b seq d_model, b seq -> b seq d_model"),
+            self.gain,
+            "... d_model, d_model -> ... d_model",
+        )
+
+        return normed.to(in_dtype)
--- tests/adapters.py
+++ tests/adapters.py
@@ -386,7 +386,12 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    from cs336_basics.modules import RMSNorm
+
+    norm = RMSNorm(d_model=d_model, eps=eps)
+    norm.load_state_dict({"gain": weights})
+
+    return norm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
